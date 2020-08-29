af6b5d674693571c2cd44906dbd2f9c135106a2d
Samar.almh <49631872+Samar-almh@users.noreply.github.com>
2020-08-29 21:49:00 +0300

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var List = listOf("A","B","C","D","E")
+
+    println("--")
+    println("the first element is:" + List.first())
+    println("--")
+    println("the last element is:" + List.last())
+    println("--")
+
+
+}
